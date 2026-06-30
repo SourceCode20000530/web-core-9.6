9b195916e73bde373459e309b9be9022096e43c4
SourceCode20000530 <sourcecode20000530@gmail.com>
2026-06-30 17:56:07 +0900

    Add swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../scss/style.scss';
 
 import * as swiperConnect from './swiper-connect';
+
 import * as menu from './menu';
 import * as mainContentShowMore from './main-content-show-more';
--- src/js/swiper-connect.js
+++ src/js/swiper-connect.js
@@ -1,4 +1,28 @@
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle'
-// register Swiper custom elements
-register()
+import Swiper from 'swiper';
+import { Navigation, Pagination} from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  }
+})
